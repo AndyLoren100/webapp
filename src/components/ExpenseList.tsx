7956664b345230f16a9698757b5a73f415252716
AndyLoren100 <gjergj224@gmail.com>
2025-08-01 17:32:19 +0200

    Completed the dashboard status

--- src/components/ExpenseList.tsx
+++ src/components/ExpenseList.tsx
@@ -1,4 +1,6 @@
 import type { Expense } from "../model/Expense";
+import CurrencyUtils from "../utils/CurrencyUtils";
+import DateUtils from "../utils/DateUtils";
 
 interface Props {
   expenses: Expense[];
@@ -6,27 +8,6 @@ interface Props {
 
 const ExpenseList = ({ expenses }: Props) => {
   return (
-    // <div>
-    //   <table border={1}>
-    //     <thead>
-    //       <tr>
-    //         <th>Title</th>
-    //         <th>Amount</th>
-    //         <th>Date</th>
-    //       </tr>
-    //     </thead>
-    //     <tbody>
-    //       {expenses.map((expense) => (
-    //         <tr key={expense.expenseId}>
-    //           <td>{expense.name}</td>
-    //           <td>{expense.amount}</td>
-    //           <td>{expense.date}</td>
-    //         </tr>
-    //       ))}
-    //     </tbody>
-    //   </table>
-    // </div>
-
     <div className="card">
       <h5 className="card-header">
         Expense
@@ -38,11 +19,13 @@ const ExpenseList = ({ expenses }: Props) => {
             <div className="d-flex justify-content-between border-bottom-1 p-3 text-dark">
               <div className="card-title m-0">
                 <h5>{expense.name}</h5>
-                <span className="fst-italic">{expense.date.toString()}</span>
+                <span className="fst-italic">
+                  {DateUtils.formatDateString(expense.date)}
+                </span>
               </div>
               <div className="card-subtitle">
                 <span className="badge rounded-pill app-primary-bg-color">
-                  {expense.amount}
+                  {CurrencyUtils.formatToUSD(expense.amount)}
                 </span>
               </div>
             </div>
