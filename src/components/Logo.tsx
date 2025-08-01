ea52ae7ebea7abf5e82faab3071a6e1f09e84c0b
AndyLoren100 <gjergj224@gmail.com>
2025-08-01 16:07:31 +0200

    completed bootstrap design

---
+++ src/components/Logo.tsx
@@ -0,0 +1,17 @@
+import logo from "../assets/react.svg";
+
+const Logo = () => {
+  return (
+    <a className="navbar-brand" href="#">
+      <img
+        src={logo}
+        alt="Logo"
+        width={48}
+        height={48}
+        className="rounded-circle"
+      />
+    </a>
+  );
+};
+
+export default Logo;
